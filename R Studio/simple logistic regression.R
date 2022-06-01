6b149981c2fc0909c7a0b061bb4b3b728bf4fd30
JAZEELA P <55051124+jazeela221b@users.noreply.github.com>
2022-06-02 01:18:12 +0530

    Delete simple logistic regression.R

--- R Studio/simple logistic regression.R
+++
@@ -1,7 +0,0 @@
-data(package = .packages(all.available = TRUE))
-# Select some columns from mtcars.
-input <- mtcars[,c("am","cyl","hp","wt")]
-print(head(input))
-#Create Regression Model
-am_data = glm(formula=am ~ cyl + hp + wt ,data=input,family=binomial)
-print(summary(am_data))
